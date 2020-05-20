088d5661e9c80a3f3fdd180e1202144c1e25cc6e
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-05-19 21:52:37 -0500

    db actualizada

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,18 +3,6 @@ import { base_url } from "./variables";
 
 axios.defaults.withCredentials = true;
 
-// export const login = (credentials) => {
-//   return axios.post(`http://localhost:3000/api/login`, credentials);
-// };
-
-// export const signup = (credentials) => {
-//   return axios.post(`http://localhost:3000/api/signup`, credentials);
-// };
-
-// export const logout = () => {
-//   return axios.post(`http://localhost:3000/api/logout`);
-// };
-
 export const login = (credentials) => {
   return axios.post(`${base_url}/login`, credentials);
 };
