cdfc7a568bb3d86a34d733c32c68c8a4226219a7
carlosgvinuesa <56604586+carlosgvinuesa@users.noreply.github.com>
2020-05-23 20:25:59 -0500

    debug faltaba un }

--- src/services/transportServices.js
+++ src/services/transportServices.js
@@ -3,10 +3,10 @@ import { base_url } from "./variables";
 
 axios.defaults.withCredentials = true;
 
-
 export const createTransport = (transport) => {
   return axios.post(`${base_url}/transports`, transport);
-  
+};
+
 export const getTransports = () => {
   return axios.get(`${base_url}/transports`);
 };
